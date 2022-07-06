faba1a9eb9dfa317cddcff94014c7f2775ce6226
Suldem <suldemka1@gmail.com>
2022-07-06 13:22:22 +0300

    60% completed order

---
+++ script/functions.js
@@ -0,0 +1,253 @@
+function orgInit(type, markerLayer, variable) {
+    for (let i = 0; i < orgdata.length; i++) {
+        let orgProp = []                                // обнуление параметров маркера
+
+        if (orgdata[i][type] == type) {
+
+            marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
+                title: orgdata[i].name          //заголовок маркера
+            })
+
+            if (orgdata[i].trosta) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/trosta.png"><div class="orgPropsText">ТОЧКА РОСТА</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+            if (orgdata[i].scoc) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/cos.png"><div class="orgPropsText">Цифровая образовательная среда</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+            if (orgdata[i].kvant) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/kvant.png"><div class="orgPropsText">Кванториум</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+            if (orgdata[i].itcube) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/itcube.png"><div class="orgPropsText">it-Куб</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+
+            if (orgdata[i].masterskaya) {
+                orgProp.push(`<div class="orgPropsContainer"><div class="orgPropsText">В этом учебном заведении есть мастерская</div>${orgdata[i].masterskaya}</div>`)
+            }
+            else {
+                orgProp.push('')
+            }
+
+            if (orgdata[i].gorpit != null) {
+                orgProp.push(`<div class="orgPropsGorpit"><div class=""></div></div>
+                  <div class="info">
+                      <input id="info__body_1" class="info__switch" type="checkbox">
+                      <label for="info__body_1" class="info__headline">Информация по организации горячего питания</label>
+                      <div class="info__body">
+                      ${orgdata[i].gorpit}
+                      </div>
+                  </div>`)
+            }
+            else {
+                orgProp.push(`<div class="orgPropsContainer"><div class="orgPropsText">Информация по организации горячего питания отсутсвует</div></div>`)
+            }
+
+            popupInfo =
+                `<div class="orgPopup">
+                  <div>${orgdata[i].name}</div>
+                  <div>Код образовательного учреждения ${orgdata[i].code}</div>
+                  <div class="orgProps">
+                      ${orgProp[0]}
+                      ${orgProp[1]}
+                      ${orgProp[2]}
+                      ${orgProp[3]}
+                      ${orgProp[4]}
+                      ${orgProp[5]}
+
+                      <a href='https://${orgdata[i].link}'>Перейти на сайт</a>
+                  </div>
+              </div>`
+
+            marker.bindPopup(popupInfo, {
+                color: '#ffffff',
+                fillColor: '#ffffff',
+                closeButton: true,
+                minWidth: 400,
+                maxWidth: 600,
+                maxHeight: 1200,
+                keepInView: true,
+                className: 'popup-sula'
+            });
+            variable.addLayer(marker)
+        }
+    }
+    markerLayer.addLayer(variable)
+}
+
+function propInit(propName, markerLayer, variable) {
+    for (let i = 0; i < orgdata.length; i++) {
+        let orgProp = []                                // обнуление параметров маркера
+
+        if (orgdata[i][propName]) {
+
+            marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
+                title: orgdata[i].name          //заголовок маркера
+            })
+
+            if (orgdata[i].trosta) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/trosta.png"><div class="orgPropsText">ТОЧКА РОСТА</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+            if (orgdata[i].scoc) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/cos.png"><div class="orgPropsText">Цифровая образовательная среда</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+            if (orgdata[i].kvant) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/kvant.png"><div class="orgPropsText">Кванториум</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+            if (orgdata[i].itcube) {
+                orgProp.push('<div class="orgPropsContainer"><img src="./assets/itcube.png"><div class="orgPropsText">it-Куб</div></div>')
+            }
+            else {
+                orgProp.push('')
+            }
+
+            if (orgdata[i].masterskaya) {
+                orgProp.push(`<div class="orgPropsContainer"><div class="orgPropsText">В этом учебном заведении есть мастерская</div>${orgdata[i].masterskaya}</div>`)
+            }
+            else {
+                orgProp.push('')
+            }
+
+            if (orgdata[i].gorpit != null) {
+                orgProp.push(`<div class="orgPropsGorpit"><div class=""></div></div>
+                  <div class="info">
+                      <input id="info__body_1" class="info__switch" type="checkbox">
+                      <label for="info__body_1" class="info__headline">Информация по организации горячего питания</label>
+                      <div class="info__body">
+                      ${orgdata[i].gorpit}
+                      </div>
+                  </div>`)
+            }
+            else {
+                orgProp.push(`<div class="orgPropsContainer"><div class="orgPropsText">Информация по организации горячего питания отсутсвует</div></div>`)
+            }
+
+            popupInfo =
+                `<div class="orgPopup">
+                  <div>${orgdata[i].name}</div>
+                  <div>Код образовательного учреждения ${orgdata[i].code}</div>
+                  <div class="orgProps">
+                      ${orgProp[0]}
+                      ${orgProp[1]}
+                      ${orgProp[2]}
+                      ${orgProp[3]}
+                      ${orgProp[4]}
+                      ${orgProp[5]}
+
+                      <a href='https://${orgdata[i].link}'>Перейти на сайт</a>
+                  </div>
+              </div>`
+
+            marker.bindPopup(popupInfo, {
+                color: '#ffffff',
+                fillColor: '#ffffff',
+                closeButton: true,
+                minWidth: 400,
+                maxWidth: 600,
+                maxHeight: 1200,
+                keepInView: true,
+                className: 'popup-sula'
+            });
+            variable.addLayer(marker)
+        }
+    }
+    markerLayer.addLayer(variable)
+}
+
+function markerInit(type, markerLayer, variable) {                                  // функция инициализации маркеров
+    if (document.getElementById(type).checked == true) {
+        orgInit(type, markerLayer, variable)
+    }
+}
+
+function FilterByType(type, markerLayer, variable) {           // добавление обработчика клика на инпуты
+
+    document.getElementById(type).onchange = function () {
+
+        if (this.checked) {
+            markerInit(type, markerLayer, variable)
+            markerLayer.addLayer(variable)
+        }
+
+        else {
+            variable.removeFrom(markerLayer)
+            variable.clearLayers()
+        }
+        console.log(this.checked)
+    }
+}
+
+function FilterByProperty(propName, markerLayer, variable) {
+
+    document.getElementById(propName).onchange = function () {
+
+        if (this.checked) {
+            propInit(propName, markerLayer, variable)
+            markerLayer.addLayer(variable)
+        }
+
+        else {
+            variable.removeFrom(markerLayer)
+            variable.clearLayers()
+        }
+
+    }
+}
+
+// function uncheck(propName) {
+
+//     var uncheck = document.getElementsByTagName('input');
+//     var prop = document.getElementById(propName)
+
+//     for (var i = 0; i < uncheck.length; i++) {
+
+//         if (uncheck[i].type == 'checkbox') {
+//             uncheck[i].checked = false
+//         }
+
+//     }
+
+//     prop.setAttribute('checked', 'true')
+// }
+
+function iconInit() {
+    trostaIcon = DG.icon({
+        iconUrl: 'assets/trosta.png',
+        iconSize: [70, 70]
+    });
+
+    scocIcon = DG.icon({
+        iconUrl: 'assets/scoc.png',
+        iconSize: [70, 70]
+    })
+
+    itcubeIcon = DG.icon({
+        iconUrl: 'assets/it-cube.png',
+        iconSize: [70, 70]
+    })
+
+    anotherIcon = DG.icon({
+        iconUrl: 'assets/icon.png',
+        iconSize: [70, 70]
+    })
+}
--- script/script.js
+++ script/script.js
@@ -1,146 +1,87 @@
 let marker, iconCaption, markers, popupInfo
-let orgProp = []
+
+let spo, school, dou, podved, dop, trosta, scoc, kvant, itcube, masterskaya
+let types = ['spo', 'school', 'dou', 'podved', 'dop']
 
 DG.then(function () {
     return DG.plugin('https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js');
 }).then(function () {
-    markers = DG.markerClusterGroup();
 
-    var map = DG.map('map', {
+    markers = DG.markerClusterGroup()
+    podved = DG.markerClusterGroup()
+    spo = DG.markerClusterGroup()
+    school = DG.markerClusterGroup()
+    dou = DG.markerClusterGroup()
+    dop = DG.markerClusterGroup()
+
+    trosta = DG.markerClusterGroup()
+    scoc = DG.markerClusterGroup()
+    kvant = DG.markerClusterGroup()
+    itcube = DG.markerClusterGroup()
+    masterskaya = DG.markerClusterGroup()
+
+    let map = DG.map('map', {                                   // инициализация и настройка карты
         center: DG.latLng(51.742136, 94.01699),
         zoom: 7,
         minZoom: 7,
         maxZoom: 18,
-        maxBounds: [[56.42569105147632, 86.74332423905955], [46.77389234789008, 103.13590236405958]],
+        maxBounds: [[56.42569105147632, 85.74332423905955], [46.77389234789008, 103.13590236405958]],
         maxBoundsViscosity: 0.1,
         zoomControl: false
     });
 
-    
+    // var obj = {
+    //     onAddToMap: function (map) {
+    //         var container = this.getContainer();
 
-    iconInit()
+    //         // Создание вложенных элементов: 
+    //         var controlMove = document.createElement('DIV');
+    //         controlMove.id = container.id + '_controlMove';
+    //         controlMove.style.backgroundColor = 'white';
+    //         controlMove.style.cursor = 'pointer';
+    //         controlMove.innerHTML = 'Moscow';
+    //         container.appendChild(controlMove);
 
-    DG.geoJson(data,
+    //         // Подписка на событие элемента управления: 
+    //         this.observers.push(this.getDgMap().addEventListener(controlMove.id, 'DgClick', function () { myMap.setCenter(new DG.GeoPoint(37.609, 55.753)); }));
+    //     },
+
+    //     onRemoveOnMap: function () {
+
+    //     },
+
+    //     getStates: function () {
+
+    //     }
+    // }
+
+    // let MyControl = DG.Controls.Abstract.extend(obj);
+
+    // // Создаем объект элемента управления:
+    // let Trosta = new MyControl();
+    // // Добавляем элемент управления на карту:
+    // map.controls.add(Trosta);
+
+    DG.geoJson(data,                                            // слой с границами кожуунов
         { color: '#ff0522', fillColor: '#708dff', fillOpacity: 0.2, stroke: 'red', weight: 1, opacity: 0.3 }).addTo(map)
 
-    for (var i = 0; i < orgdata.length; i++) {
+    markerInit('spo', markers, spo)
+    markerInit('school', markers, school)
+    markerInit('dop', markers, dop)
+    markerInit('dou', markers, dou)
+    markerInit('podved', markers, podved)
 
-        switch (orgdata[i].type) {
-            case "podved":
-                marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
-                    title: orgdata[i].name
-                });
-                break;
+    map.addLayer(markers)                                       //добавление слоя с маркерами на карту
 
-            case "dop":
-                marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
-                    title: orgdata[i].name
-                });
-                break;
-            case "school":
-                marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
-                    title: orgdata[i].name
-                });
-                break;
-            case "dou":
-                marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
-                    title: orgdata[i].name
-                });
-                break;
-            case "spo":
-                marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
-                    title: orgdata[i].name
-                })
-                break;
-            default:
-                marker = DG.marker([orgdata[i].k1, orgdata[i].k2], {
-                    title: orgdata[i].name
-                });
-                console.log('default');
-        }
+    FilterByType('spo', markers, spo)
+    FilterByType('school', markers, school)
+    FilterByType('dop', markers, dop)
+    FilterByType('dou', markers, dou)
+    FilterByType('podved', markers, podved)
 
-        orgProp = []
-        if (orgdata[i].type != "podved" && orgdata[i].type != "min") {
-            if (orgdata[i].trosta == "YES") {
-                orgProp.push('<div class="orgPropsContainer"><img src="./assets/trosta.png"><div class="orgPropsText">ТОЧКА РОСТА</div></div>')
-            }
-            if (orgdata[i].scoc == "YES") {
-                orgProp.push('<div class="orgPropsContainer"><img src="./assets/cos.png"><div class="orgPropsText">Цифровая образовательная среда</div></div>')
-            }
-            if (orgdata[i].kvant == "YES") {
-                orgProp.push('<div class="orgPropsContainer"><img src="./assets/kvant.png"><div class="orgPropsText">Кванториум</div></div>')
-            }
-            if (orgdata[i].itcube == "YES") {
-                orgProp.push('<div class="orgPropsContainer"><img src="./assets/itcube.png"><div class="orgPropsText">it-Куб</div></div>')
-            }
-
-            if (orgdata[i].masterskaya) {
-                orgProp.push(`<div class="orgPropsContainer"><div class="orgPropsText">В этом учебном заведении есть мастерская</div>${orgdata[i].masterskaya}</div>`)
-            }
-
-            if (orgdata[i].gorpit != null) {
-                orgProp.push(`<div class="orgPropsGorpit"><div class=""></div></div>
-                <div class="info">
-                    <input id="info__body_1" class="info__switch" type="checkbox">
-                    <label for="info__body_1" class="info__headline">Информация по организации горячего питания</label>
-                    <div class="info__body">
-                    ${orgdata[i].gorpit}
-                    </div>
-                </div>`)
-            }
-            else {
-                orgProp.push(`<div class="orgPropsContainer"><div class="orgPropsText">Информация по организации горячему питанию отсутсвует</div></div>`)
-            }
-        }
-        else {
-            orgdata[i].code = "отсуствует"
-        }
-
-        popupInfo =
-            `<div class="orgPopup">
-                <div>${orgdata[i].name}</div>
-                <div>Код образовательного учреждения ${orgdata[i].code}</div>
-                <div class="orgProps">
-                    ${orgProp}
-                    ${console.log(orgProp)}
-                    <a href='https://${orgdata[i].link}'>Перейти на сайт</a>
-                </div>
-            </div>`
-
-
-        marker.bindPopup(popupInfo, {
-            color: '#ffffff',
-            fillColor: '#ffffff',
-            closeButton: true,
-            minWidth: 400,
-            maxWidth: 600,
-            maxHeight: 1200,
-            keepInView: true,
-            className: 'popup-sula'
-        });
-        markers.addLayer(marker);
-    }
-    map.addLayer(markers);
+    FilterByProperty('trosta', markers, trosta)
+    FilterByProperty('scoc', markers, scoc)
+    FilterByProperty('kvant', markers, kvant)
+    FilterByProperty('itcube', markers, itcube)
+    FilterByProperty('masterskaya', markers, masterskaya)
 });
-
-function iconInit() {
-    trostaIcon = DG.icon({
-        iconUrl: 'assets/trosta.png',
-        iconSize: [70, 70]
-    });
-
-    scocIcon = DG.icon({
-        iconUrl: 'assets/scoc.png',
-        iconSize: [70, 70]
-    })
-
-    itcubeIcon = DG.icon({
-        iconUrl: 'assets/it-cube.png',
-        iconSize: [70, 70]
-    })
-
-    anotherIcon = DG.icon({
-        iconUrl: 'assets/icon.png',
-        iconSize: [70, 70]
-    })
-}
